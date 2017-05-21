9932a3d342eb06e66ec9c75107bd9fb4ddf16c6e
Alex Giacobbe <agiacobbe18@gmail.com>
2017-05-20 21:10:35 -0400

    create migration files for tours, galleries, users, and pictures

---
+++ db/migrate/20170520164123_create_galleries.rb
@@ -0,0 +1,12 @@
+class CreateGalleries < ActiveRecord::Migration[5.0]
+  def change
+    create_table :galleries do |t|
+      t.string :name
+      t.string :description
+      t.integer :cover
+      t.integer :tour_id, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
