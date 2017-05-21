b0548c3e7849473b029770d250eea6112d4bd051
Alex Giacobbe <agiacobbe18@gmail.com>
2017-05-21 09:26:39 -0400

    add user logic to create method

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -14,6 +14,8 @@ class ToursController < ApplicationController
 
   def create
     @tour = Tour.new(tour_params)
+    @tour.user_id = current_user.id
+    binding.pry
     if @tour.save
       flash.notice = "#{@tour.name} Created!"
       redirect_to tours_path(@tour)
@@ -21,5 +23,11 @@ class ToursController < ApplicationController
       render 'new'
     end
   end
+
+  private
+    def tour_params
+      params.require(:tour).permit(:name, :location, :date, :tourers, :description, :user_id)
+    end
+
 
 end
