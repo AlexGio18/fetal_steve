a602dd30693c9ed48ecc62365b8bca03f32ca96c
Alex Giacobbe <agiacobbe18@gmail.com>
2017-05-20 21:07:40 -0400

    add all routes for tours, galleries, and pictures

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'fetalsteves#index'
   get 'story', :to => 'stories#index'
-  get 'tours', :to => 'tours#index'
+  resources :tours
+  resources :galleries
+  resources :pictures
 end
